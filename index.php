ad06206621cbaf95879b2ccb848c95d86e61e039
juanfelipecp <juanfelipecp@users.noreply.github.com>
2019-08-26 17:55:11 -0500

    JuanFelipeCp
    cree el logo y los botones de login y register

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MangaQueen</title>
+    <div id="header">
+
+
+  </head>
+  <center>
+  <body background="img/fondo.png" >
+    <table>
+    <tr>
+      <td>
+        <a href="index.php"><img src="img/logo.png" alt=""></a>
+      </td>
+      <td><a href="login.php"><h3>Logearse</h3></a></td> <br>
+      <td><a href="register.php"><h3>Registrarse</h3></a></td>
+      <td><a href="info.php">mas informacion</td>
+    </tr>
+
+  </table>
+
+  </div>
+</center>
+    
+  </body>
+</html>
+<a href="#"></a>
