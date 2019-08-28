6026999df7972df3a4627d5f6d5a6d1b350c354b
juanfelipecp <juanfelipecp@users.noreply.github.com>
2019-08-28 15:26:14 -0500

    juanfelipecp
    acabe el validar

--- validar.php
+++ validar.php
@@ -1,1 +1,34 @@
-En Proceso :(((((((((((  F
+<?php
+incluce("conexion.php")
+
+$nombre=$_POST['username'];
+$contra=$_POST['password'];
+
+  $con=conectar();
+  $SQL="SELECT COUNT(*) AS cu FROM usuario
+  WHERE username ='$nombre'AND password='$contra' ";
+  $resul=mysqli_query($con,$SQL);
+  while($file=mysquli_fetch_array($resul)){
+    if ($file['cu']==0) {
+      $si = FALSE;
+    }
+else {
+  $si = TRUE;
+  $sql2="SELECT * FROM usuario
+  WHERE username='$nombre' AND password='$contra' ";
+  $resul2=mysqli_query($con,$sql2);
+  $file2=mysquli_fetch_array($resul2)
+  $id = $file2["id"];
+          header("location:index_sesion_iniciada.php?id=$id");
+
+}
+if ($si == FALSE) {
+  header("location:index.php");
+
+}
+
+  }
+
+
+
+ ?>
